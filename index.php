50ecd5aeb9979720a18f2a5624cfa02f86c8b8aa
TALEX1995 <tanase.alex@libero.it>
2023-07-18 16:13:36 +0200

    Added form

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 include 'data/data.php';
 
-$hotel_info = array_keys($hotels[0]);
+$hotel_data = array_keys($hotels[0]);
+
+
 
 ?>
 
@@ -18,43 +20,62 @@ $hotel_info = array_keys($hotels[0]);
 </head>
 
 <body>
-    <!-- <?php foreach ($hotels as $hotel) : ?>
-        <ul>
-            <?php foreach ($hotel as $info => $data) : ?>
-                <li><strong><?= $info ?></strong> : <?= $data ?></li>
-            <?php endforeach ?>
-        </ul>
-    <?php endforeach ?> -->
 
     <!-- Table with Hotels -->
-    <table class="table">
-        <thead>
-            <tr>
-                <?php foreach ($hotel_info as $info) : ?>
-                    <th scope="col"><?= str_replace('_', ' ', ucfirst($info)) ?></th>
-                <?php endforeach ?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($hotels as $hotel) : ?>
+
+    <div class="container">
+
+
+        <form action="index.php" class="my-5" method="GET">
+            <!-- Parking -->
+            <label for="parking">Parcheggio</label>
+            <input id="parking" type="checkbox" name="parking" value="true">
+
+            <!-- Hotel vote -->
+            <select class="form-select my-3" aria-label="Default select example" name="hotel-vote">
+                <option selected>Cerca l'hotel in base al voto</option>
+                <option value="1">Uno</option>
+                <option value="2">Due</option>
+                <option value="3">Tre</option>
+                <option value="4">Quattro</option>
+                <option value="5">Cinque</option>
+            </select>
+
+
+            <button type="submit">Filtra</button>
+        </form>
+
+
+        <table class="table">
+            <thead>
                 <tr>
-                    <?php foreach ($hotel as $data => $info) : ?>
-
-                        <?php if (($data === 'parking') && ($info === true)) : ?>
-                            <td>Yes</td>
-
-                        <?php elseif (($data === 'parking') && ($info === false)) : ?>
-                            <td>No</td>
-
-                        <?php else : ?>
-                            <td><?= $info ?></td>
-                        <?php endif ?>
-
+                    <?php foreach ($hotel_data as $data) : ?>
+                        <th scope="col"><?= str_replace('_', ' ', ucfirst($data)) ?></th>
                     <?php endforeach ?>
                 </tr>
-            <? endforeach ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel) : ?>
+                    <tr>
+                        <?php foreach ($hotel as $data => $info) : ?>
+
+                            <?php if (($data === 'parking') && ($info === true)) : ?>
+                                <td>Yes</td>
+
+                            <?php elseif (($data === 'parking') && ($info === false)) : ?>
+                                <td>No</td>
+
+                            <?php else : ?>
+                                <td><?= $info ?></td>
+                            <?php endif ?>
+
+                        <?php endforeach ?>
+                    </tr>
+                <? endforeach ?>
+            </tbody>
+        </table>
+
+    </div>
 </body>
 
 </html>
